ade70a64d29dd1d9cd53062a77ab95dd1834dd66
Diego Elias <diego_eliasc2505@live.com>
2022-02-07 17:39:42 -0500

    Modificacion del Dashboard

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,30 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { getDashboardAccess } from '../../services/dashboard.service';
+import { toast } from 'react-hot-toast'
 
 export default function Dashboard() {
+
+    const nav = useNavigate()
+    const [Success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        getDashboardAccess()
+            .then(a => {
+                if (!a.success) {
+                    toast.error(a.msg)
+                    return nav('/', { replace: true })
+                }
+
+                setSuccess(a.success)
+            })
+            .catch(e => {
+                toast.error(e.message)
+                nav('/', { replace: true })
+            })
+    })
+
     return (
         <>
-            <p class=" h1 text-center ">DashBoard de nuestra empresa</p>
-            <br /><br /><br />
-            <div class="d-grid gap-2">
-                <button class="btn btn-info" type="button">
-                    <Link to={'/dashboard/products'}>
-                        Productos - Cantidades - Ingresos
-                    </Link>
-                </button>
-                <br /><br />
-                <button class="btn btn-info" type="button">
-                    <Link to={'/dashboard/users'}>
-                        Usuarios Registrados
-                    </Link>
-                </button>
-                <br /><br />
-                <button class="btn btn-info" type="button">
-                    <Link to={'/dashboard/sales'}>
-                        Comparacion de Ventas
-                    </Link>
-                </button>
-            </div>
+            {
+                Success &&
+                <>
+                    <p class=" h1 text-center ">DashBoard de nuestra empresa</p>
+                    <br /><br /><br />
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-info" type="button">
+                            <Link to={'/dashboard/products'}>
+                                Productos - Cantidades - Ingresos
+                            </Link>
+                        </button>
+                        <br /><br />
+                        <button class="btn btn-info" type="button">
+                            <Link to={'/dashboard/users'}>
+                                Usuarios Registrados
+                            </Link>
+                        </button>
+                        <br /><br />
+                        <button class="btn btn-info" type="button">
+                            <Link to={'/dashboard/sales'}>
+                                Comparacion de Ventas
+                            </Link>
+                        </button>
+                    </div>
+                </>
+            }
+
         </>
     );
 }
--- src/services/dashboard.service.js
+++ src/services/dashboard.service.js
@@ -3,27 +3,56 @@ import axios from 'axios'
 const API_URL = 'http://localhost:3001/api/dashboard'
 
 export const getDataSales = async () => {
-    const { data } = await axios.get(`${API_URL}/sales`);
+    const { data } = await axios.get(`${API_URL}/sales`, {
+        headers: {
+            authorization: `bearer ${window.sessionStorage.getItem('tkn_ad')}`
+        }
+    });
     return data;
 }
 
 export const getDataDash = async () => {
-    const { data } = await axios.get(`${API_URL}/amount`)
+    const { data } = await axios.get(`${API_URL}/amount`, {
+        headers: {
+            authorization: `bearer ${window.sessionStorage.getItem('tkn_ad')}`
+        }
+    })
     return data
 }
 
-export const getUserMonthDash = async (m,y) => {
-    const { data } = await axios.get(`${API_URL}/users/date?m=${m}&y=${y}`)
+export const getUserMonthDash = async (m, y) => {
+    const { data } = await axios.get(`${API_URL}/users/date?m=${m}&y=${y}`, {
+        headers: {
+            authorization: `bearer ${window.sessionStorage.getItem('tkn_ad')}`
+        }
+    })
     return data
 }
 
 export const getAllUsersDash = async () => {
-    const { data } = await axios.get(`${API_URL}/users`)
+    const { data } = await axios.get(`${API_URL}/users`, {
+        headers: {
+            authorization: `bearer ${window.sessionStorage.getItem('tkn_ad')}`
+        }
+    })
     return data
-    
+
 }
 
 export const getSalesCompareDash = async () => {
-    const { data } = await axios.get(`${API_URL}/sales/compare`)
+    const { data } = await axios.get(`${API_URL}/sales/compare`, {
+        headers: {
+            authorization: `bearer ${window.sessionStorage.getItem('tkn_ad')}`
+        }
+    })
+    return data
+}
+
+export const getDashboardAccess = async () => {
+    const { data } = await axios.post(API_URL, null, {
+        headers: {
+            authorization: `bearer ${window.sessionStorage.getItem('tkn_ad')}`
+        }
+    })
     return data
 }
